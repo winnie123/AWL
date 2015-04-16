5a59f853569ca235ecf9ccdba5a85619dc758bfb
winnie123 <jxs_555@126.com>
2015-04-16 21:24:36 +0800

    创建项目

---
+++ MvcApplication7/App_Start/FilterConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using MvcApplication7.InterFace.Filter;
+
+namespace MvcApplication7
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            //filters.Add(new AuthorizationGateFilter());
+            filters.Add(new HandleErrorAttribute());
+           
+        }
+    }
+}
